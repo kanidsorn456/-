ba869da0164f7fda5e904fc95d48430713e074e3
kanidsorn456 <sekpopy@gmail.com>
2024-07-18 18:27:29 +0700

    Add files via upload

---
+++ image_upload_ajax/2/index2.php
@@ -0,0 +1,184 @@
+<html>
+    <head>
+        <title>ข้อมูลวัตถุมงคล</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.11.5/datatables.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.11.5/datatables.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <style>
+        </style>
+    </head>
+    <body class="bg-dark">
+        <div class="container mt-5 pt-3">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-11">
+                            <h1>ข้อมูลวัตถุมงคล</h1>
+                        </div>
+                        <div class="col-md-1 text-end">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                        <table id="user_data" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th width="10%">รูปภาพ</th>
+                                    <th width="35%">ขื่อวัตถุมงคล</th>
+                                    <th width="35%">รายละเอียดวัตถุมงคล</th>
+                                    <th width="10%">แก้ไข</th>
+                                    <th width="10%">ลบ</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+<div id="userModal" class="modal fade">
+    <div class="modal-dialog">
+        <form method="post" id="user_form" enctype="multipart/form-data">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">เพิ่ม วัตถุมงคล</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div>
+                        <label>เพิ่ม ชื่อวัตถุมงคล</label>
+                        <input type="text" name="first_name" id="first_name" class="form-control"/>                     
+                    </div>
+                    
+                    <div>
+                        <label>เพิ่มรายละเอียดวัตถุมงคล</label>
+                        <input type="text" name="last_name" id="last_name" class="form-control"/>
+                    </div>
+                    
+                    <div>
+                        <label>เลือกรูปภาพ</label>
+                        <input type="file" name="user_image" id="user_image" class="form-control">
+                        <span id="user_uploaded_image"></span>  
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden" name="user_id" id="user_id" />
+                    <input type="hidden" name="operation" id="operation" />
+                    <input type="submit" name="action" id="action" class="btn btn-success" value="Add" />
+                    <button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<script type="text/javascript" language="javascript" >
+$(document).ready(function(){
+    $('#add_button').click(function(){
+        $('#user_form')[0].reset();
+        $('.modal-title').text("Add User");
+        $('#action').val("Add");
+        $('#operation').val("Add");
+        $('#user_uploaded_image').html('');
+    });
+    
+    var dataTable = $('#user_data').DataTable({
+        "processing":true,
+        "serverSide":true,
+        "order":[],
+        "ajax":{
+            url:"fetch.php",
+            type:"POST"
+        },
+        columnDefs: [{
+                    "defaultContent": "-",
+                    "targets": "_all"
+            },
+        ],
+    });
+    $(document).on('submit', '#user_form', function(event){
+        event.preventDefault();
+        var firstName = $('#first_name').val();
+        var lastName = $('#last_name').val();
+        var extension = $('#user_image').val().split('.').pop().toLowerCase();
+        if(extension != '')
+        {
+            if(jQuery.inArray(extension, ['gif','png','jpg','jpeg']) == -1)
+            {
+                alert("Invalid Image File");
+                $('#user_image').val('');
+                return false;
+            }
+        }   
+        if(firstName != '' && lastName != '')
+        {
+            $.ajax({
+                url:"insert.php",
+                method:'POST',
+                data:new FormData(this),
+                contentType:false,
+                processData:false,
+                success:function(data)
+                {
+                    alert(data);
+                    $('#user_form')[0].reset();
+                    $('#userModal').modal('hide');
+                    dataTable.ajax.reload();
+                }
+            });
+        }
+        else
+        {
+            alert("Both Fields are Required");
+        }
+    });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    $(document).on('click', '.delete', function(){
+        var user_id = $(this).attr("id");
+        if(confirm("Are you sure you want to delete this?"))
+        {
+            $.ajax({
+                url:"delete.php",
+                method:"POST",
+                data:{user_id:user_id},
+                success:function(data)
+                {
+                    alert(data);
+                    dataTable.ajax.reload();
+                }
+            });
+        }
+        else
+        {
+            return false;   
+        }
+    });
+});
+</script>
---
+++ image_upload_ajax/db.php
@@ -0,0 +1,5 @@
+<?php
+    $username = 'root';
+    $password = '';
+    $connection = new PDO( 'mysql:host=localhost;dbname=wathuanearn_amulet', $username, $password );
+?>
